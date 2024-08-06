f77a438e4353b02416f4dd88d72b7f7ee3b89d9f
c23005 <c23005@std.it-college.ac.jp>
2024-08-06 10:37:48 +0900

    猫がMasterでも動くように少し変更した

--- Team2/Assets/Scripts/GameControllerscript.cs
+++ Team2/Assets/Scripts/GameControllerscript.cs
@@ -1,16 +1,42 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GameControllerscript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    GameObject[] cats;
+    GameObject[] movePoss;
+    List<int> numbers = new List<int>();
+    int startCatInt;
+    List<int> CatInt = new List<int>();
+    CatMoveScript[] script = new CatMoveScript[5];
+    private void Awake()
+    {
+        cats = GameObject.FindGameObjectsWithTag("cat");
+        movePoss = GameObject.FindGameObjectsWithTag("catMovePos");
+        for(int i = 0; i < movePoss.Length; i++)
+        {
+            numbers.Add(i);
+        }
+        for(int i = 0;i < cats.Length; i++)
+        {
+            CatInt.Add(i);
+            script[i] = cats[i].GetComponent<CatMoveScript>();
+        }
+        numbers = numbers.OrderBy(a => Guid.NewGuid()).ToList();
+        for(int i = 0;i < cats.Length;i++)
+        {
+            script[i].nextInt = numbers[i];
+        }
+    }
     void Start()
     {
         Application.targetFrameRate = 60;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         
